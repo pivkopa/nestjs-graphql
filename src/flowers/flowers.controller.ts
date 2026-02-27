677cf2fdeb31c4eabb413b893b8d4720f445f8a8
Dmytro Pivkopa <dmytro.pivkopa@cowchain.io>
2026-02-27 15:28:44 +0200

    add-tests-middlewares

--- src/flowers/flowers.controller.ts
+++ src/flowers/flowers.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FlowersService } from './flowers.service';
+import { ParseIntPipe } from 'src/conception/pipe';
+import { AuthGuard } from 'src/conception/guard';
+import { LogingInterceptor } from 'src/conception/interceptor';
 
 @Controller('flowers')
+@UseInterceptors(LogingInterceptor)
 export class FlowersController {
-  constructor(private readonly flowersService: FlowersService) {}
+  constructor(private readonly flowersService: FlowersService) { }
 
   @Get('')
+  @UseGuards(AuthGuard)
   findAll() {
     return this.flowersService.findAll();
   }
